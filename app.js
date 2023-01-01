a96104f6d22a0dd26e1045cd5044c0ba575097dd
dkolotouros <dkolotouros1@gmail.com>
2022-12-31 20:15:05 -0500

    teapot and custom cookie endpoint

--- app.js
+++ app.js
@@ -5,5 +5,10 @@ const port = 4000;
 app.get('/', (req, res) => {
   res.send("Hello World!")
 });
+ 
+app.get('/teapot', (req, res) => {
+  res.set("X-tea", true).status(418).send('im a little teapot')
+});
+ 
 
 app.listen(port, () => console.log(`app listening on port ${port}`))
